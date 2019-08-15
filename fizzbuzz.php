947e8d051fa7fdd3c847a64a448ac753f888dd82
knd0100 <52206772+knd0100@users.noreply.github.com>
2019-08-15 18:58:40 +0900

    Add files via upload
    PHP言語 Fizzbuzz解答

---
+++ fizzbuzz.php
@@ -0,0 +1,17 @@
+$count = 1;
+while ($count <= 100){
+
+if(($count % 3 == 0) and ($count % 5 == 0)){
+
+					echo "FizzBuzz";
+}elseif($count % 3 == 0 ){
+				echo "Fizz";
+}elseif($count % 5 == 0){
+			echo "Buzz";
+}else{
+		echo $count;
+}
+$count++;
+	echo "<br>";
+	
+}
