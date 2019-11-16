3882eb48600840007bc95e5c71666c7ee444dd66
Ogm <52206772+ogm0100@users.noreply.github.com>
2019-11-16 17:22:36 +0900

    Create fizzbuzz.cpp

---
+++ fizzbuzz.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define rep(i,n)for(int i=0;i<n;i++)
+using namespace std;
+
+void Main(){
+
+}
+
+int main(){
+  rep(i,100){
+      if (i % 15 == 0 ) {
+        cout << "Fizz,Buzz" << endl;
+      } else if ( i % 3 == 0 ) {
+        cout << "Fizz" << endl;
+      } else if ( i % 5 == 0 ) {
+        cout<< "Buzz" << endl;
+      } else {
+        cout << i << endl;
+      }
+   }
+}
