977bda47d0d85ca910f64e2afa54aa48111578da
Ogm <52206772+ogm0100@users.noreply.github.com>
2019-11-16 19:06:49 +0900

    Create fizzbuzz.pl

---
+++ fizzbuzz.pl
@@ -0,0 +1,11 @@
+for (my $i = 1; $i <= 100; $i++) {
+  if ($i % 3 == 0 && $i % 5 == 0) {  
+    print "Fizz,Buzz\n";
+  } elsif ($i % 3 == 0) {  
+    print "Fizz\n";
+  } elsif ($i % 5 == 0) {  
+    print "Buzz\n";
+  } else {  
+    print $i . "\n";
+  }
+}
