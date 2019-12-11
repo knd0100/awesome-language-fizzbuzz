9a35b377f8586fcff214abc9c4ed0c9df485e45c
Ogm <52206772+ogmeat@users.noreply.github.com>
2019-12-11 12:54:58 +0900

    Update fizubuzz.js

--- fizubuzz.js
+++ fizubuzz.js
@@ -1,16 +1,16 @@
-<script>
-    function fizzbuzz(){
+function fizzbuzz(){
+    
     for(i=1; i<=100; i++){
-    if(i%15 === 0){
-                console.log('FizzBuzz');
-            }else if(i%3 === 0){
-            console.log('Fizz');
-        }else if(i%5 === 0){
+    if (i%15 === 0) {
+        console.log('FizzBuzz');
+    } else if(i%3 === 0) {
+        console.log('Fizz');
+    } else if(i%5 === 0){
         console.log('Buzz');
-    }else{
-    console.log(i);
-            }
-        }
+    } else {
+        console.log(i);
+       }
     }
-    fizzbuzz();
-</script>
+ }
+
+fizzbuzz();
