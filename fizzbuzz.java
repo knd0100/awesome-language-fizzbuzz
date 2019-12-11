a6ba8f6b52168d31b4caaf45ca88e49bfc2318f7
Ogm <52206772+ogmeat@users.noreply.github.com>
2019-12-11 13:02:45 +0900

    Update fizzbuzz.java

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -3,11 +3,11 @@ public class FizzBuzz {
     for (int i = 1; i < 101; i++) {
       if (i % 15 == 0) {
         System.out.println("FizzBuzz");
-              } else if (i % 5 == 0) {
+      } else if (i % 5 == 0) {
         System.out.println("Buzz");
-            } else if (i % 3 == 0) {
+      } else if (i % 3 == 0) {
         System.out.println("Fizz");
-          } else {
+      } else {
         System.out.println(i);
       }
     }
