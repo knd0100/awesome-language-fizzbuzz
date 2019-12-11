2f7d4781a64592a588fa0d0a8bcacbf2a7f7e853
Ogm <52206772+ogmeat@users.noreply.github.com>
2019-12-11 13:04:52 +0900

    Update fizzbuzz.php

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,17 +1,15 @@
 $count = 1;
-while ($count <= 100){
+   while ($count <= 100){
 
-if(($count % 3 == 0) and ($count % 5 == 0)){
-
-					echo "FizzBuzz";
-}elseif($count % 3 == 0 ){
-				echo "Fizz";
-}elseif($count % 5 == 0){
-			echo "Buzz";
-}else{
-		echo $count;
-}
-$count++;
-	echo "<br>";
-	
+   if(($count % 3 == 0) and ($count % 5 == 0)){
+	echo "FizzBuzz";
+   } elseif ($count % 3 == 0 ){
+	echo "Fizz";
+   } elseif ($count % 5 == 0){
+	echo "Buzz";
+   } else {
+	echo $count;
+   }
+	$count++;
+   echo "<br>";	
 }
