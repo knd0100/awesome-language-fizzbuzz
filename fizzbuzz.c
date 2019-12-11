32925b2017169ee64f3e9bf67d7f4a419ad28986
Ogm <52206772+ogmeat@users.noreply.github.com>
2019-12-11 12:56:29 +0900

    Update fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 
 int main(void){
-  
   int i;
-
   for (i = 1; i <= 100; i++) {
-  if (i % 3 == 0 && i % 5 == 0)
-              printf("Fizz,Buzz\n");
-            else if (i % 3 == 0)
-          printf("Fizz\n");
-        else if (i % 5 == 0)
-      printf("Buzz\n");
-    else  
-  printf("%d\n", i);
-  }
-return 0;
+      if (i % 3 == 0 && i % 5 == 0)
+        printf("Fizz,Buzz\n");
+      else if (i % 3 == 0)
+        printf("Fizz\n");
+      else if (i % 5 == 0)
+        printf("Buzz\n");
+      else  
+        printf("%d\n", i);
+    }
 }
