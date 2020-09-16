a9ac7c3877e60e8feb9c6aa2e9a56b98926676a0
Ogm <52206772+ogmeat@users.noreply.github.com>
2020-09-16 09:35:40 +0900

    Update fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 for i in 1..100
-    if i%15 == 0
+    if i % 15 == 0
         puts "FizzBuzz!"
-    elsif i%3 == 0
+    elsif i % 3 == 0
         puts "Fizz!"
-    elsif i%5 == 0 
+    elsif i % 5 == 0 
         puts "Buzz!"
     else
         puts i
