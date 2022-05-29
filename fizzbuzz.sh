e2d39f8906cdd6df894ab6999ac7752316cdd364
Ogm <watershed@keemail.me>
2022-05-29 16:10:43 +0900

    Create fizzbuzz.sh

---
+++ fizzbuzz.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for i in {1..100} ; do
+    if ((i % 15 == 0)); then
+        echo fizzbuzz
+    elif ((i % 3 == 0)); then
+        echo fizz
+    elif ((i % 5 == 0)); then
+        echo buzz
+    else
+        echo "$i"
+    fi
+done
