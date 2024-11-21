68e9c0c951f0b2d7e09ad373a90baf7426de3546
Ogmer <watershed@keemail.me>
2024-11-22 03:54:22 +0900

    Update fizzbuzz.lua

--- fizzbuzz.lua
+++ fizzbuzz.lua
@@ -1,4 +1,3 @@
--- fizzbuzz関数を更新
 function fizzbuzz(i)
   if i % 15 == 0 then
     return "FizzBuzz"
@@ -12,6 +11,5 @@ function fizzbuzz(i)
 end
 
 for i = 1, 100 do
-  -- 既存の条件文を削除し、以下を追加
   print(fizzbuzz(i))
 end
