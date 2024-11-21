7295700e4828dd1a04303fcfbdee5580478b9330
Ogmer <watershed@keemail.me>
2024-11-22 03:54:45 +0900

    Update fizzbuzz.php

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,6 +1,5 @@
 <?php
 
-// fizzbuzz関数を追加
 function fizzbuzz($count) {
     if (($count % 3 == 0) and ($count % 5 == 0)) {
         return 'FizzBuzz';
@@ -15,7 +14,6 @@ function fizzbuzz($count) {
 
 $count = 1;
 while ($count <= 100) {
-    // 既存の条件文を削除し、以下を追加
     echo fizzbuzz($count);
     ++$count;
     echo PHP_EOL;
