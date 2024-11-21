4f26b40cf60b492c628651e871ffb4a090367aa8
Ogmer <watershed@keemail.me>
2024-11-22 03:54:05 +0900

    Update fizzbuzz.java

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,12 +1,10 @@
 public class Main {
   public static void main (String[] args) {
     for (int i = 1; i <= 100; i++) {
-      // 既存の条件文を削除し、以下を追加
       System.out.println(fizzBuzz(i));
     }
   }
 
-  // fizzBuzzメソッドを追加
   public static String fizzBuzz(int i) {
     if (i % 15 == 0) {
       return "FizzBuzz";
