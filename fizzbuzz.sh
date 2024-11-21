7a21ae0406cd14044d002260c405cad16b4f8c03
Ogmer <watershed@keemail.me>
2024-11-22 03:56:00 +0900

    Update fizzbuzz.sh

--- fizzbuzz.sh
+++ fizzbuzz.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# fizzbuzz関数を追加
 fizzbuzz() {
     if (( $1 % 15 == 0 )); then
         echo fizzbuzz
@@ -14,6 +13,5 @@ fizzbuzz() {
 }
 
 for i in {1..100}; do
-    # 既存の条件文を削除し、以下を追加
     fizzbuzz $i
 done
