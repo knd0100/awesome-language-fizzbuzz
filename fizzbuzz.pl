ece02d56fc0af038707287060180d445df9dd148
Ogmer <watershed@keemail.me>
2024-11-22 03:55:07 +0900

    Update fizzbuzz.pl

--- fizzbuzz.pl
+++ fizzbuzz.pl
@@ -3,7 +3,6 @@
 use strict;
 use warnings;
 
-# fizzbuzzサブルーチンを追加
 sub fizzbuzz {
     my ($i) = @_;
     if ($i % 15 == 0) {
@@ -18,6 +17,5 @@ sub fizzbuzz {
 }
 
 for my $i (1..100) {
-    # 既存の条件文を削除し、以下を追加
     print fizzbuzz($i) . "\n";
 }
